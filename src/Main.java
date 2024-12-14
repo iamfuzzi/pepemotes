215062e3b22afac6a71096f51a8d8915d57f31c7
iamfuzzi <geometryposter@g,ail.com>
2024-12-15 00:00:01 +0300

    Основаня программа

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Тестовый класс");
-        int x = 18;
+        SwingUtilities.invokeLater(() -> new Window());
+        new Replacer();
     }
 }
